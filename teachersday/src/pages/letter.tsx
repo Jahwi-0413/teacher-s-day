30cf6b703716634976a587e275995902ed24f017
jaehe <jaehe0413@naver.com>
2023-05-15 23:39:33 +0900

    letter 화면

--- teachersday/src/pages/letter.tsx
+++ teachersday/src/pages/letter.tsx
@@ -1,3 +1,29 @@
+import style from "../style/letter.module.css";
+
+import flower from "../asset/flower.png";
+
 export default function Letter() {
-  return (<></>)
+  return (
+    <>
+      <div className={style.container}>
+        <img src={flower} className={style.topFlower} alt="카네이션" />
+        <img src={flower} className={style.bottomFlower} alt="카네이션" />
+        <div className={style.letterBox}>
+          <div className={style.text}>
+            <span>to. 계희쌤께</span>
+            <div>
+              한학기동안 가르쳐주셔서 <br />
+              너무 감사해요!!
+              <br />
+              멋진 개발자가 되어서
+              <br />
+              돌아올게요
+              <br />
+              <br /> 사랑해요 ♥
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
