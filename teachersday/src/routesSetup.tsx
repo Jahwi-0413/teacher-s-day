ce9abb54c598b484d5b33c6140132acc58c12144
jaehe <jaehe0413@naver.com>
2023-05-22 20:48:44 +0900

    routing by hashrouting

--- teachersday/src/routesSetup.tsx
+++ teachersday/src/routesSetup.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom"
-import Letter from "./pages/letter"
-import Main from "./pages/main"
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Letter from "./pages/letter";
+import Main from "./pages/main";
 export default function routesSetup() {
   return (
     <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='/letter' element={<Letter />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/letter" element={<Letter />} />
     </Routes>
-  )
+  );
 }
