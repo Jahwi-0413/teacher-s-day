b51b53395a4cb492a844c1512b73d8346f020b2e
jaehe <jaehe0413@naver.com>
2023-05-23 09:36:17 +0900

    routing에 basename 추가

--- teachersday/src/App.tsx
+++ teachersday/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import RoutesSetup from './routesSetup';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import RoutesSetup from "./routesSetup";
 
 function App() {
   return (
-    <BrowserRouter> <RoutesSetup></RoutesSetup></BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <RoutesSetup></RoutesSetup>
+    </BrowserRouter>
   );
 }
 
